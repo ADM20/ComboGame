1ffc05ff1ac28e28f520078102f7267e072bc574
ADM20 <40206116@live.napier.ac.uk>
2019-04-09 13:03:32 +0100

    Menu Sytem working

--- combo_game/scenes/scene_menu.cpp
+++ combo_game/scenes/scene_menu.cpp
@@ -3,32 +3,48 @@
 #include "../game.h"
 #include <SFML/Window/Keyboard.hpp>
 #include <iostream>
+#include <system_renderer.h>
+
 
 using namespace std;
 using namespace sf;
+
 
 void MenuScene::Load() {
   cout << "Menu Load \n";
   {
     auto txt = makeEntity();
-    auto t = txt->addComponent<TextComponent>("Platformer\nPress Space to Start \n1 - Load Test Level \n2 - Options");
+    auto t = txt->addComponent<TextComponent>("Platformer \n1 - Load Test Level \n2 - Options");
   }
   setLoaded(true);
 }
 
 void MenuScene::Update(const double& dt) {
-  // cout << "Menu Update "<<dt<<"\n";
+	bool flag = true;
+	RenderWindow &window = Engine::GetWindow();
+	while (flag)
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Num1:
+					Engine::ChangeScene(&level3);
+					flag = false;
+					break;
 
-  if (sf::Keyboard::isKeyPressed(Keyboard::Space)) {
-    Engine::ChangeScene(&level1);
-  }
-  //Start Game
-  else if (sf::Keyboard::isKeyPressed(Keyboard::Num1)) {  
-	  Engine::ChangeScene(&level3);
-  }
-  //Options Menu
-  else if (sf::Keyboard::isKeyPressed(Keyboard::Num2)) {
-	  Engine::ChangeScene(&optionsMenu);
-  }
+				case sf::Keyboard::Num2:
+					Engine::ChangeScene(&optionsMenu);
+					flag = false;
+					break;
+					// Don't forget to check for other events here
+				}
+			}
+		}
+	}
   Scene::Update(dt);
 }
--- combo_game/scenes/scene_options.cpp
+++ combo_game/scenes/scene_options.cpp
@@ -3,6 +3,7 @@
 #include "../game.h"
 #include <SFML/Window/Keyboard.hpp>
 #include <iostream>
+#include <system_renderer.h>
 
 using namespace std;
 using namespace sf;
@@ -11,15 +12,40 @@ void OptionsScene::Load() {
   cout << "Options Load \n";
   {
     auto txt = makeEntity();
-    auto t = txt->addComponent<TextComponent>("Options Menu \n -- Add options here --\n 2 - Menu");
+    auto t = txt->addComponent<TextComponent>("Options Menu \n\n 1 - Keybinds \n 2 - Resolution \n  \n 3 - Menu");
   }
   setLoaded(true);
 }
 
 void OptionsScene::Update(const double& dt) {
   //cout << "options Update "<<dt<<"\n";
-	if (Keyboard::isKeyPressed(Keyboard::Num2)) {
-		Engine::ChangeScene(&menu);
+	bool flag = true;
+	RenderWindow &window = Engine::GetWindow();
+	while (flag)
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::KeyReleased:
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Num1:
+					//call keybind scene
+					flag = false;
+					break;
+				case sf::Keyboard::Num2:
+					//call resolution selection scene
+					flag = false;
+					break;
+				case sf::Keyboard::Num3:
+					Engine::ChangeScene(&menu);
+					flag = false;
+					break;
+				}
+			}
+		}
 	}
   Scene::Update(dt);
 }
