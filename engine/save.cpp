983549e8563fff430417218689f0dc836aacb35f
Daniel Korsah <dkorsah15@aol.co.uk>
2019-04-18 15:17:53 +0100

    first itteration of save/load

--- engine/save.cpp
+++ engine/save.cpp
@@ -2,12 +2,23 @@
 
 void SaveSystem::read()
 {
+	std::ifstream infile("save.txt");
 
+	//if save file exists
+	if (infile.good)
+	{
+		int level;
+		while (infile >> level)
+		{
+			SaveSystem::levelUnlocked = level;
+		}
+	}
 }
 
 void SaveSystem::write()
 {
-
+	std::ofstream outfile("save.txt");
+	outfile << SaveSystem::levelUnlocked;
 }
 
 int SaveSystem::levelUnlocked = 0;
