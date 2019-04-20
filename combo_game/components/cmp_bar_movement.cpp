a81e0e31665e623d0de226789c498beabe7daf4f
ADM20 <40206116@live.napier.ac.uk>
2019-04-20 12:43:04 +0100

    visual bar added

--- combo_game/components/cmp_bar_movement.cpp
+++ combo_game/components/cmp_bar_movement.cpp
@@ -6,17 +6,16 @@ using namespace sf;
 using namespace std;
 
 
-
 void BarMovementComponent::update(double dt)
 {
-
+		move(-0.5, 0);	
 }
 
 BarMovementComponent::BarMovementComponent(Entity* p)
     : _speed(100.0f), Component(p) {}
 
 bool BarMovementComponent::validMove(const sf::Vector2f& pos) {
-   return (LevelSystem::getTileAt(pos) != LevelSystem::WALL);
+   return (LevelSystem::getTileAt(pos) != LevelSystem::TEMPO);
   // return true;
 }
 
@@ -24,6 +23,10 @@ void BarMovementComponent::move(const sf::Vector2f& p) {
   auto pp = _parent->getPosition() + p;
   if (validMove(pp)) {
     _parent->setPosition(pp);
+  }
+  else
+  {
+	  _parent->setForDelete();
   }
 }
 
--- combo_game/scenes/scene_level1.cpp
+++ combo_game/scenes/scene_level1.cpp
@@ -15,6 +15,7 @@ static shared_ptr<Entity> player;
 static shared_ptr<Entity> bar;
 static shared_ptr<Entity> tempo;
 static shared_ptr<Entity> marker;
+static double tempoTime = .5;
 
 void Level1Scene::Load() {
   std::cout << " Scene 1 Load" << endl;
@@ -44,17 +45,12 @@ void Level1Scene::Load() {
 	  s->setShape<sf::RectangleShape>(Vector2f(900, 30));
 	  s->getShape().setFillColor(Color::Blue);
 
-	  bar = makeEntity();
-	  bar->setPosition(ls::getTilePosition(ls::findTiles(ls::BAR)[0]));
-	  auto b = bar->addComponent<ShapeComponent>();
-	  b->setShape<sf::RectangleShape>(Vector2f(10,40));
-	  b->getShape().setFillColor(Color::Magenta);
-	  bar->addComponent<BarMovementComponent>();
+	  
 	  
 	  marker = makeEntity();
 	  marker->setPosition(ls::getTilePosition(ls::findTiles(ls::TEMPO)[0]));
 	  auto t = marker->addComponent<ShapeComponent>();
-	  t->setShape<sf::RectangleShape>(Vector2f(10, 60));
+	  t->setShape<sf::RectangleShape>(Vector2f(40, 60));
 	  t->getShape().setFillColor(Color::White);
 
 	   
@@ -75,7 +71,7 @@ void Level1Scene::UnLoad() {
 }
 
 void Level1Scene::Update(const double& dt) {
-
+	Scene::Update(dt);
   if (ls::getTileAt(player->getPosition()) == ls::END) {
     Engine::ChangeScene((Scene*)&level2);
   }
@@ -87,7 +83,20 @@ void Level1Scene::Update(const double& dt) {
 	//}
   //Engine::ChangeScene((Scene*)&level2);
   //}
-  Scene::Update(dt);
+  static double barTime = 0.0f;
+  barTime -= dt;
+
+  if (barTime <= 0.f)
+  {
+	  barTime = tempoTime;
+	  bar = makeEntity();
+	  bar->setPosition(ls::getTilePosition(ls::findTiles(ls::BAR)[0]));
+	  auto b = bar->addComponent<ShapeComponent>();
+	  b->setShape<sf::RectangleShape>(Vector2f(10, 40));
+	  b->getShape().setFillColor(Color::Magenta);
+	  bar->addComponent<BarMovementComponent>();
+  }
+  
 }
 
 void Level1Scene::Render() {
